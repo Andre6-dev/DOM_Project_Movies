9522031bee199e4f1396565a4df3ba042c2307e9
Andre <andre.gallegos@tecsup.edu.pe>
2022-03-15 19:49:44 -0500

    Controlling the backdrop functionality in our page.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,12 +5,32 @@ const addMovieModal = document.getElementById('add-modal')
 // const startaAddMovieButton = document.querySelector('header').lastElementChild;
 const startaAddMovieButton = document.querySelector('header button')
 
+const backdrop = document.getElementById('backdrop')
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive')
+
 console.log(addMovieModal)
 console.log(startaAddMovieButton)
+
+
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible')
+};
 
 //Change the class to the modal to toggle visibility
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible')
+    toggleBackdrop()
 }
+
+const cancelAddMovie = () => {
+    toggleMovieModal()
+};
+
+const backdropClickHandler = () => {
+    toggleMovieModal()
+}
+
 // We don't to add parenthesis because we don't want to execute it immediately
-startaAddMovieButton.addEventListener('click', toggleMovieModal)
+startaAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', toggleMovieModal);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
