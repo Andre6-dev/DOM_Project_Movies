8cb295638df94cc50532fc30fbe8801378b26f63
Andre <andre.gallegos@tecsup.edu.pe>
2022-03-16 13:38:41 -0500

    Deleting Movie Elements.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,11 +11,50 @@ const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 // Selecting all the modal's input.
 const userInputs = addMovieModal.querySelectorAll('input')
 
+const entryTextSection = document.getElementById('entry-text')
+
 const movies = []
 
 console.log(addMovieModal)
 console.log(startaAddMovieButton)
 console.log(userInputs)
+
+const updateUI = () => {
+    if (movies.length === 0) {
+        entryTextSection.style.display = 'block';
+    } else {
+        entryTextSection.style.display = 'none';
+    }
+};
+
+const deleteMovieHandler = (movieId) => {
+    let movieIndex = 0;
+    for (const movie of movies) {
+        if (movie.id === movieId) {
+            break;
+        }
+        movieIndex++
+    }
+    movies.splice(movieIndex, 1);
+    const listRoot = document.getElementById('movie-list')
+    listRoot.children[movieIndex].remove();
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
+    const newMovieElement = document.createElement('li')
+    newMovieElement.className = 'movie-element'
+    newMovieElement.innerHTML =
+        `<div class="movie-element__image">
+            <img src="${imageUrl}" alt="${title}">
+        </div>
+        <div class="movie-element__info">
+            <h2>${title}</h2>
+            <p>${rating}/5 stars</p>
+        </div>`
+    newMovieElement.addEventListener('click', deleteMovieHandler.bind(null, id))
+    const listRoot = document.getElementById('movie-list')
+    listRoot.append(newMovieElement)
+};
 
 
 const toggleBackdrop = () => {
@@ -55,6 +94,7 @@ const addMovieHandler = () => {
     }
 
     const newMovie = {
+        id: Math.random().toString(),
         title: titleValue,
         image: imageUrlValue,
         rating: ratingValue
@@ -64,6 +104,8 @@ const addMovieHandler = () => {
     console.log(movies)
     toggleMovieModal()
     clearMovieInput()
+    renderNewMovieElement(newMovie.id, newMovie.title, newMovie.image, newMovie.rating)
+    updateUI()
 };
 
 
